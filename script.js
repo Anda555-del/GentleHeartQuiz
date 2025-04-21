90762d89cef1bfdb1c8b42ce1f4ae1ffc925d632
Anda555-del <anda555skibidi@gmail.com>
2025-04-22 00:15:56 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const result = document.getElementById("result");
+
+  document.querySelectorAll("button").forEach((button) => {
+    button.addEventListener("click", function () {
+      const value = button.getAttribute("data-value");
+
+      if (value === "1") {
+        result.textContent = "คุณมีแนวโน้มที่จะปกป้องใจตัวเองได้ดีในสถานการณ์กระทบใจ";
+      } else if (value === "2") {
+        result.textContent = "คุณมีวิจารณญาณและสามารถเข้าใจตัวเองในระหว่างเสพสื่อ";
+      } else if (value === "3") {
+        result.textContent = "คุณอาจต้องการพื้นที่อ่อนโยนและการสนับสนุนทางใจเพิ่มเติม";
+      }
+    });
+  });
+});
